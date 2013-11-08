0cce6ad641faa334babd0142f99ebce8eed823f7
Stefano Salvatori <ssalvatori@gmail.com>
2013-11-07 22:24:12 -0300

    added store data in mongodb

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import sys
 import urllib
 import urllib2
 import re
+import pymongo
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 '''
   create url
@@ -68,13 +70,30 @@ def getCuotas(date, managerId, maxType):
 
   return data
 
-def storeData(data):
-  return 1
 
+'''
+store data on mongo database
+'''
+def storeData(data):
+  
+  try :
+    MONGODB_URI = 'mongodb://USUARIO:PASSWORD@HOST:PORT/DB'
+    client = pymongo.MongoClient(MONGODB_URI)
+    db = client.get_default_database()
+    db['dailyValues'].insert(data)
+  except BaseException, e:
+    print "Error "+e
+
+
+
+
+'''
+  get info from asociacion de fondos mutuos
+'''
 def getInfo(dataFM, date, maxType):
   for managerId, fmNames in dataFM.items():
     rawData = getCuotas(date, managerId, maxType)
-    print findFM(rawData, fmNames)
+    return findFM(rawData, fmNames, date)
 
 '''
   load fm data from xml
@@ -93,20 +112,32 @@ def loadFMInfo():
 
   return result
 
-def findFM(data, fmNames):
+
+'''
+
+'''
+def findFM(data, fmNames, dateObtain):
+  dateCreate = datetime.now().strftime('%Y-%m-%d')
   result = []
   print "** Searching for"
   print fmNames
   for name in fmNames:
     for row in data:
       if re.match(row['name'],name):
+        row['name'] = name
+        row['price'] = row['price'].replace('.','')
+        row['price'] = float(row['price'].replace(',','.'))
+        row['create'] = dateCreate
+        row['obtain'] = dateObtain 
         result.append(row)
 
   return result
+
 
 if __name__ == '__main__':
   date = sys.argv[1]
   maxType = 10
   dataFM = loadFMInfo()
-  getInfo(dataFM, date, maxType)
+  dataParsed = getInfo(dataFM, date, maxType)
+  storeData(dataParsed)
 
